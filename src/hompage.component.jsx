b9872648b07f0d49188166cb3f30642efd5d94fc
xlboy360 <48699121+xlboy360@users.noreply.github.com>
2021-09-22 13:50:59 -0500

    Created project with create react app and template pwa support and added files for homepage

---
+++ src/hompage.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./homepage.styles.scss";
+
+function HomePage() {
+  return (
+    <div className="home-page">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Title</h1>
+            <span className="subtitle">SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
